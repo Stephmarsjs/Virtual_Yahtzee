9d766c9af0336c8dfae13f9dd40a148d2679010d
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-09-21 13:09:34 -0400

    initial commit

---
+++ src/com/Game/Die.java
@@ -0,0 +1,20 @@
+package com.Game;
+
+import java.util.Random;
+
+public class Die {
+    private int sides = 6;
+    private int value;
+
+    public Die() {
+        this.value = 1;
+    }
+
+    public void roll(Random rand) {
+        value = rand.nextInt(sides) + 1;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/com/Game/Hand.java
@@ -0,0 +1,25 @@
+package com.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Hand {
+    private List<Die> dice = dice = new ArrayList<Die>();
+
+    public Hand(int size) {
+        for (int count = 0; count < size; count++) {
+            dice.add(new Die());
+
+        }
+
+    }
+
+    public void roll(Random rand) {
+        dice.get(0).roll(rand);
+    }
+
+    public List<Die> egtDice() {
+        return dice;
+    }
+}
